cbe8f5715922594cbd9368ab5876a077952632b8
dat13899 <datel1389@gmail.com>
2024-11-06 14:53:41 +0700

    up

---
+++ firebase-config.ts
@@ -0,0 +1,8 @@
+export const firebase_config = {
+  apiKey: "AIzaSyBaOpQKmXlYtaHKDTWESV_JTHXjc0riVbw",
+  authDomain: "boring-realtime.firebaseapp.com",
+  projectId: "boring-realtime",
+  storageBucket: "boring-realtime.firebasestorage.app",
+  messagingSenderId: "321409841040",
+  appId: "1:321409841040:web:96907f880f9d8e32686515"
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { initializeApp } from 'firebase/app';
+import { firebase_config } from '../../firebase-config';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,4 +11,6 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'boring-realtime';
+  firebase_config = firebase_config;
+  // app = initializeApp(firebase_config);
 }
